910dd3da70395cd8d1c1d56caa6d2a9d220f44f3
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-04-27 16:51:22 +0200

    Jour 4

--- AddressBook/src/AppBundle/Controller/SocieteController.php
+++ AddressBook/src/AppBundle/Controller/SocieteController.php
@@ -4,33 +4,133 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Societe;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/societes")
+ * Societe controller.
+ *
+ * @Route("societes")
  */
 class SocieteController extends Controller
 {
     /**
-     * @Route("/")
+     * Lists all societe entities.
+     *
+     * @Route("/", name="societes_index")
+     * @Method("GET")
      */
-    public function listAction()
+    public function indexAction()
     {
-        return $this->redirectToRoute('app_contact_list');
-    }
+        $em = $this->getDoctrine()->getManager();
 
-    /**
-     * @Route("/{id}")
-     */
-    public function showAction($id)
-    {
-        $repo = $this->getDoctrine()->getRepository(Societe::class);
+        $societes = $em->getRepository('AppBundle:Societe')->findAll();
 
-        $societe = $repo->find($id);
-
-        return $this->render('AppBundle:Societe:show.html.twig', array(
-            'societe' => $societe
+        return $this->render('societe/index.html.twig', array(
+            'societes' => $societes,
         ));
     }
 
+    /**
+     * Creates a new societe entity.
+     *
+     * @Route("/new", name="societes_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $societe = new Societe();
+        $form = $this->createForm('AppBundle\Form\SocieteType', $societe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($societe);
+            $em->flush();
+
+            return $this->redirectToRoute('societes_show', array('id' => $societe->getId()));
+        }
+
+        return $this->render('societe/new.html.twig', array(
+            'societe' => $societe,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a societe entity.
+     *
+     * @Route("/{id}", name="societes_show")
+     * @Method("GET")
+     */
+    public function showAction(Societe $societe)
+    {
+        $deleteForm = $this->createDeleteForm($societe);
+
+        return $this->render('societe/show.html.twig', array(
+            'societe' => $societe,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing societe entity.
+     *
+     * @Route("/{id}/edit", name="societes_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Societe $societe)
+    {
+        $deleteForm = $this->createDeleteForm($societe);
+        $editForm = $this->createForm('AppBundle\Form\SocieteType', $societe);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('societes_edit', array('id' => $societe->getId()));
+        }
+
+        return $this->render('societe/edit.html.twig', array(
+            'societe' => $societe,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a societe entity.
+     *
+     * @Route("/{id}", name="societes_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Societe $societe)
+    {
+        $form = $this->createDeleteForm($societe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($societe);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('societes_index');
+    }
+
+    /**
+     * Creates a form to delete a societe entity.
+     *
+     * @param Societe $societe The societe entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Societe $societe)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('societes_delete', array('id' => $societe->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
 }
